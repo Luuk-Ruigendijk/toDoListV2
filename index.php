2457d2eb4459594d9b7ac7465a3c5f50d80aa540
luck4you <31469918+Luuk-Ruigendijk@users.noreply.github.com>
2020-03-20 10:04:46 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <html>
 	<head>
 		<title>To Do List</title>
+		<link rel="stylesheet" type="text/css" href="style.css">
 	</head>
 	<body>
 		<p>this is a list of things to do.</p>
--- script.js
+++ script.js
@@ -3,14 +3,28 @@ var listNumber = 0;
 function addList(){
 	var makeList = document.createElement("div");
 	var listItem = document.createElement("p");
+	var addTask = document.createElement("button");
+	var renameList = document.createElement("button");
 	var removeList = document.createElement("button");
-	listItem.innerText = "dit is de lijst nummer " + (listNumber+1);
+	var listName = prompt("Please enter the name of this list", "list number "+(listNumber+1));
+	if (listName != null) {
+		listItem.innerText = listName;
+	};
+	if (listName == null || listName == "") {
+		listItem.innerText = "unnamed list.";
+	};
+	//listItem.innerText = "dit is de lijst nummer " + (listNumber+1);
 	removeList.innerText = "Remove list";
 	removeList.setAttribute("onclick", "removeMyList(" + listNumber + ")");
+	renameList.innerText = "Rename list";
+	renameList.setAttribute("onclick", "renameMyList(" + listNumber + ")");
 	document.getElementById("listGroup").appendChild(makeList);
 	makeList.appendChild(listItem);
+	makeList.appendChild(addTask);
+	makeList.appendChild(renameList);
 	makeList.appendChild(removeList);
 	makeList.setAttribute("id", "list" + listNumber);
+	makeList.setAttribute("class", "list");
 	listNumber++;
 }
 
@@ -24,4 +38,15 @@ function removeMyList(removeList) {
 		removeableList.removeChild(removeableList.firstChild);
 	}
 	removeableList.remove();
+}
+
+function renameMyList(whichList) {
+	let renameableList = document.getElementById("list"+whichList).getElementsByTagName('p')[0];
+	let newListName = prompt("Please enter the new name of this list", "enter new name here");
+	if (newListName != null && newListName != "") {
+		renameableList.innerText = newListName;
+	};
+	if (newListName == null || newListName == "") {
+		alert("please enter something to change the list name.");
+	};
 }
