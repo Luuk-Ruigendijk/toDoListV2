2725d489ac4ad790aadc707b2ab647f4eca845f9
luck4you <99046268@mydavinci.nl>
2020-04-17 13:13:29 +0200

    added code for connecting to the database

---
+++ backend.php
@@ -0,0 +1,22 @@
+<?php
+
+//echo "this is an echo from the server...";
+//echo $_GET['name'];
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=PHPMetSlemmer", $username, $password);	//maak een supermarkt
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    //echo "Connected successfully";
+    }
+catch(PDOException $e)
+    {
+    //echo "Connection failed: " . $e->getMessage();
+    }
+
+    
+?>
