9dfaad3e52217fccc41e61a2e02cfb1ba3154ebe
luck4you <99046268@mydavinci.nl>
2020-04-22 14:21:13 +0200

    fixed the connection to the database, and it now sends a reply with all lists

--- backend.php
+++ backend.php
@@ -8,7 +8,7 @@ $username = "root";
 $password = "";
 
 try {
-    $conn = new PDO("mysql:host=$servername;dbname=PHPMetSlemmer", $username, $password);	//maak een supermarkt
+    $conn = new PDO("mysql:host=$servername;dbname=todolist", $username, $password);	//maak een supermarkt
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     //echo "Connected successfully";
@@ -18,5 +18,23 @@ catch(PDOException $e)
     //echo "Connection failed: " . $e->getMessage();
     }
 
-    
+    $sql = 'SELECT * FROM lists';
+
+    $statement = $conn->prepare($sql);	
+
+    $statement->execute();
+
+    $lists = $statement->fetchAll(PDO::FETCH_CLASS);
+
+    foreach($lists as $list){
+    	$task1 = (object) array('name' => 'bar');
+    	$task2 = (object) array('name' => 'foe');
+    	$list->tasks = [$task1, $task2];
+    }
+
+    $responseText = json_encode($lists);
+
+	header("Content-Type: application/json");
+
+	echo $responseText;
 ?>
--- index.php
+++ index.php
@@ -10,6 +10,28 @@
 		<div id="addList">
 			<button onclick="addList()">Add list</button>
 		</div>
-		<script type="text/javascript" src="script.js"></script>
+		<script>
+			function addList() {
+				console.log("addDoc");
+			    var xhttp = new XMLHttpRequest();
+			    xhttp.onreadystatechange = function() {
+			    if (this.readyState == 4 && this.status == 200) {
+			    		console.log(this.response);
+				    	lists = JSON.parse(this.response);
+				    	console.dir(lists);
+				    	for(list of lists){
+				    		let el = document.createElement('p');
+				    		el.innerHTML = list.listname;
+				    		document.getElementById('listGroup').appendChild(el);
+				    	}
+				    	console.log("onreadystatechange");
+				    	//console.dir(this);
+				        //document.getElementById("demo").innerHTML = this.responseText;
+				    }
+				};
+			    xhttp.open("GET", "backend.php", true);
+			    xhttp.send();
+			}
+		</script>
 	</body>
 </html>
